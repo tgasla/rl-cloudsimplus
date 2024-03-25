3be031cd9715b7d04e50e77b30ecde2a82949be6
TheodorosAslanidis <theodoros.aslanidis@ucdconnect.ie>
2024-03-25 23:56:41 +0000

    added code in HostWithoutCreatedList.java

--- cloudsimplus-gateway/src/main/java/daislab/cspg/HostWithoutCreatedList.java
+++ cloudsimplus-gateway/src/main/java/daislab/cspg/HostWithoutCreatedList.java
@@ -1,3 +1,35 @@
-public class HostWithoutCreatedList {
-    
+package daislab.cspg;
+
+import org.cloudbus.cloudsim.hosts.HostSimple;
+import org.cloudbus.cloudsim.provisioners.ResourceProvisioner;
+import org.cloudbus.cloudsim.resources.Pe;
+import org.cloudbus.cloudsim.vms.Vm;
+
+import java.util.List;
+
+public class HostWithoutCreatedList extends HostSimple {
+    public HostWithoutCreatedList(List<Pe> peList) {
+        super(peList);
+    }
+
+    public HostWithoutCreatedList(List<Pe> peList, boolean activate) {
+        super(peList, activate);
+    }
+
+    public HostWithoutCreatedList(ResourceProvisioner ramProvisioner, ResourceProvisioner bwProvisioner, long storage, List<Pe> peList) {
+        super(ramProvisioner, bwProvisioner, storage, peList);
+    }
+
+    public HostWithoutCreatedList(long ram, long bw, long storage, List<Pe> peList) {
+        super(ram, bw, storage, peList);
+    }
+
+    public HostWithoutCreatedList(long ram, long bw, long storage, List<Pe> peList, boolean activate) {
+        super(ram, bw, storage, peList, activate);
+    }
+
+    @Override
+    protected void addVmToCreatedList(Vm vm) {
+        // do nothing to avoid accumulating vm data
+    }
 }
