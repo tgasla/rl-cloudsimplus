618410b5eeb2e762ba3a09fadf20f8272d9ae556
Theodoros Aslanidis <theodoros.aslanidis@ucdconnect.ie>
2024-04-02 19:54:53 +0100

    merged pretrain_save.py and load_retrain.py into one python script, run.py. Also, better structure for utils.py where swf_reader is now inside this module. Added check to load replay buffer also, if any

---
+++ rl-manager/mnt/run.py
@@ -0,0 +1,242 @@
+import os
+from datetime import datetime
+import argparse
+import json
+import numpy as np
+import gymnasium as gym
+import gym_cloudsimplus
+import torch
+
+import stable_baselines3 as sb3
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
+# from stable_baselines3.common import results_plotter
+# from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+import dummy_agents
+from utils import FilenameFormatter, SWFReader
+
+def datetime_to_str():
+    return datetime.now().strftime("%y%m%d-%H%M%S")
+
+learning_rate_dict = {
+    "DQN": "0.00005",
+    "DDPG": "0.0001",
+    "A2C": "0.0002",
+    "PPO": "0.0001"
+}
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--pretrain-env", 
+    type=str,
+    choices=["SmallDC-v0", "LargeDC-v0"],
+    help="The environment to pretrain the agent on"
+)
+parser.add_argument(
+    "--algo", 
+    type=str,
+    choices=[
+        "DQN", "A2C", "PPO", 
+        "RNG", "DDPG", "HER", 
+        "SAC", "TD3"
+    ],
+    help="The RL algorithm that is used for training"
+)
+parser.add_argument(
+    "--pretrain-timesteps",
+    type=int,
+    help="The number of timesteps to pretrain"
+)
+parser.add_argument(
+    "--transfer-env",
+    type=str,
+    help="The environment to transfer the agent after pretraining"
+)
+parser.add_argument(
+    "--transfer-timesteps",
+    type=int,
+    help="The number of timesteps to perform after the environment transfer"
+)
+args = parser.parse_args()
+algorithm_str = str(args.algo).upper()
+pretrain_env = str(args.pretrain_env)
+pretrain_timesteps = int(args.pretrain_timesteps)
+transfer_env = str(args.transfer_env)
+transfer_timesteps = int(args.transfer_timesteps)
+
+experiment_id = FilenameFormatter.create_filename_id(
+    algorithm_str,
+    pretrain_env,
+    pretrain_timesteps
+)
+
+timestamp = datetime_to_str()
+
+filename_id = timestamp + "_" + experiment_id
+
+# Read jobs
+jobs = SWFReader.swf_read("mnt/LLNL-Atlas-2006-2.1-cln.swf", jobs_to_read=25)
+
+# Create eval dir
+eval_log_dir = "./eval-logs/"
+os.makedirs(eval_log_dir, exist_ok=True)
+
+eval_log_path = (
+    f"{eval_log_dir}"
+    f"{filename_id}"
+    f"_monitor.csv"
+)
+
+# Create tensorboard dir
+tb_log_dir = "./tb-logs/"
+os.makedirs(tb_log_dir, exist_ok=True)
+
+# Create model storage dir
+model_storage_dir = "./model-storage/"
+os.makedirs(model_storage_dir, exist_ok=True)
+
+model_storage_path = (
+    f"{model_storage_dir}"
+    f"{filename_id}"
+)
+
+# Create and wrap the environment
+env = gym.make(
+    pretrain_env,
+    jobs_as_json=json.dumps(jobs),
+    simulation_speedup="10000",
+    split_large_jobs="true",
+    render_mode="ansi"
+)
+
+# Monitor needs the environment to have a render_mode set
+# If render_mode is None, it will give a warning.
+env = Monitor(
+    env, 
+    eval_log_path, 
+    info_keywords=("cost", "validCount")
+)
+
+# Add some action noise for exploration
+n_actions = env.action_space.shape[-1]
+action_noise = NormalActionNoise(
+    mean=np.zeros(n_actions), 
+    sigma=0.1 * np.ones(n_actions)
+)
+
+# Select the appropriate algorithm
+if algorithm_str == "RNG":
+    algorithm = getattr(dummy_agents, algorithm_str)
+    policy = "RngPolicy"
+else:
+    algorithm = getattr(sb3, algorithm_str)
+    policy = "MlpPolicy"
+
+# Instantiate the agent
+model = algorithm(
+    policy=policy,
+    env=env,
+    verbose=True,
+    tensorboard_log=tb_log_dir,
+    # TODO: for now, all action noise is ignored in RNG algorithm
+    action_noise=action_noise,
+    device=device
+)
+
+# Train the agent
+model.learn(
+    total_timesteps=pretrain_timesteps,
+    tb_log_name=filename_id,
+    log_interval=1
+)
+
+# Save the agent
+model.save(model_storage_path)
+
+# Save the replay buffer if the algorithm has one
+if (issubclass(algorithm, OffPolicyAlgorithm)):
+    model.save_replay_buffer(model_storage_path + "_RP")
+
+# Close the environment and free the resources
+env.close()
+
+# Delete model
+del model
+
+new_experiment_id = FilenameFormatter.create_filename_id(
+    algorithm_str,
+    pretrain_env,
+    pretrain_timesteps,
+    transfer_env,
+    transfer_timesteps
+)
+
+new_filename_id = timestamp + "_" + new_experiment_id
+
+new_eval_log_path = (
+    f"{eval_log_dir}"
+    f"{new_filename_id}"
+    f"_monitor.csv"
+)
+
+new_tb_log_name = new_filename_id
+
+# Create and wrap the environment
+new_env = gym.make(
+    transfer_env,
+    jobs_as_json=json.dumps(jobs),
+    simulation_speedup="10000",
+    split_large_jobs="true",
+    render_mode="ansi"
+)
+
+# Monitor needs the environment to have a render_mode set
+# If render_mode is None, it will give a warning.
+new_env = Monitor(
+    new_env, 
+    new_eval_log_path, 
+    info_keywords=("cost", "validCount")
+)
+
+# Load the trained agent
+model = algorithm.load(
+    model_storage_path,
+    device=device,
+    tensorboard_log=tb_log_dir,
+    env=new_env
+)
+
+# Load the replay buffer if the algorithm has one
+if (issubclass(algorithm, OffPolicyAlgorithm)): 
+   model.load_replay_buffer(model_storage_path + "_RP")
+
+# Set the learning rate to a small initial value
+model.learning_rate = learning_rate_dict.get(algorithm_str)
+
+# Retrain the agent initializing the weights from the saved agent
+model.learn(
+    total_timesteps= transfer_timesteps,
+    # The right thing to do is to set reset_num_timesteps=True
+    # This way, the learning restarts
+    # The only problem is that tensorboard recognizes
+    # it as a new model, but that's not a critical issue for now
+    reset_num_timesteps=True,
+    tb_log_name=new_tb_log_name,
+    log_interval=1
+)
+
+# Save the new model
+new_model_path = (
+    f"{model_storage_dir}"
+    f"{new_filename_id}"
+)
+
+model.save(new_model_path)
+
+# Save the new replay buffer if the algorithm has one
+if (issubclass(algorithm, OffPolicyAlgorithm)): 
+    model.save_replay_buffer(new_model_path + "_RP")
--- rl-manager/mnt/utils.py
+++ rl-manager/mnt/utils.py
@@ -1,27 +1,89 @@
-from datetime import datetime
-
-def get_filename_id(env_id, algorithm_str, timesteps, retraining_env=None):
-    filename_id = (
-        f"p{env_id}"
-        f"_{algorithm_str}"
-        f"_{millify(timesteps)}"
-        # f"{datetime_to_str()}"
-    )
-    if retraining_env is not None:
+class FilenameFormatter(object):
+    @staticmethod
+    def _millify(num):
+        num = float(f"{num:.3f}")
+        magnitude = 0
+        suffix = ['', 'K', 'M', 'B', 'T']
+        while abs(num) >= 1000:
+            magnitude += 1
+            num /= 1000
+        return f"{num:.0f}" + suffix[magnitude]
+    
+    @staticmethod
+    def create_filename_id(
+        algorithm_str, 
+        pretrain_env, 
+        pretrain_timesteps,
+        transfer_env=None,
+        transfer_timesteps=None
+    ):
         filename_id = (
-            f"{filename_id}"
-            f"_r{retraining_env}"
+            f"{algorithm_str}"
+            f"_p{pretrain_env}"
+            f"_{FilenameFormatter._millify(pretrain_timesteps)}"
         )
-    return filename_id
+        if transfer_env is not None:
+            assert transfer_timesteps is not None
 
-def datetime_to_str():
-    return datetime.now().strftime("%m%d%y_%H%M%S")
+            filename_id = (
+                f"{filename_id}"
+                f"_r{transfer_env}"
+                f"_{FilenameFormatter._millify(transfer_timesteps)}"
+            )
+        return filename_id
 
-def millify(num):
-    num = float(f"{num:.3f}")
-    magnitude = 0
-    suffix = ['', 'K', 'M', 'B', 'T']
-    while abs(num) >= 1000:
-        magnitude += 1
-        num /= 1000
-    return f"{num:.0f}" + suffix[magnitude]
+ 
+class SWFReader(object):
+    """
+        Format as specified in
+        http://www.cs.huji.ac.il/labs/parallel/workload/swf.html
+    """
+    @staticmethod
+    def _as_cloudlet_descriptor_dict(
+        job_id,
+        submit_time,
+        run_time,
+        mips,
+        allocated_cores
+    ):
+        return {
+            'jobId': job_id,
+            'submissionDelay': submit_time,
+            'mi': run_time * mips * allocated_cores,
+            'numberOfCores': allocated_cores,
+        }
+    
+    @staticmethod
+    def swf_read(filename, jobs_to_read=None):
+        jobs = []
+        with open(filename, 'r') as f:
+            jobs_read = 0
+            for line in f.readlines():
+                if line.startswith(';'):
+                    continue
+                if jobs_to_read is not None and \
+                    jobs_read == jobs_to_read:
+                    return jobs
+
+                line_stripped = line.strip()
+                line_splitted = line_stripped.split()
+
+                job_id = int(line_splitted[0])
+                submit_time = int(line_splitted[1])
+                # wait_time = int(line_splitted[2])
+                run_time = int(line_splitted[3])
+                allocated_cores = int(line_splitted[4])
+                status = int(line_splitted[10])
+                mips = 1250
+
+                if (run_time > 0 and allocated_cores > 0 and status == 0):
+                    cloudlet = SWFReader._as_cloudlet_descriptor_dict(
+                        job_id,
+                        submit_time,
+                        run_time,
+                        mips,
+                        allocated_cores
+                    )
+                    jobs.append(cloudlet)
+                    jobs_read += 1
+        return jobs
