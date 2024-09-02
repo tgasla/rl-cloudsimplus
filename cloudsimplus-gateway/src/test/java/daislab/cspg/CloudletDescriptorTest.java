596c2c5ab7537d6ce511f06b365ee20e7caf477e
TheodorosAslanidis <theodoros.aslanidis@ucdconnect.ie>
2024-09-02 21:20:15 +0100

    moved logback logs into log/ dir

--- cloudsimplus-gateway/src/test/java/daislab/cspg/CloudletDescriptorTest.java
+++ cloudsimplus-gateway/src/test/java/daislab/cspg/CloudletDescriptorTest.java
@@ -1,58 +1,58 @@
-package daislab.cspg;
+// package daislab.cspg;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterAll;
+// import com.google.gson.Gson;
+// import com.google.gson.reflect.TypeToken;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.AfterAll;
 
-import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.List;
-import java.io.File;
-import org.apache.commons.io.FileUtils;
+// import java.lang.reflect.Type;
+// import java.util.Arrays;
+// import java.util.List;
+// import java.io.File;
+// import org.apache.commons.io.FileUtils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CloudletDescriptorTest {
+// public class CloudletDescriptorTest {
 
-    private final Gson gson = new Gson();
+// private final Gson gson = new Gson();
 
-    @Test
-    public void serializesDeserializesToJSON() {
-        final CloudletDescriptor cloudletDescriptor = new CloudletDescriptor(1, 2, 3, 4);
+// @Test
+// public void serializesDeserializesToJSON() {
+// final CloudletDescriptor cloudletDescriptor = new CloudletDescriptor(1, 2, 3, 4);
 
-        final String asJson = gson.toJson(cloudletDescriptor);
-        final CloudletDescriptor deserialized = gson.fromJson(asJson, CloudletDescriptor.class);
+// final String asJson = gson.toJson(cloudletDescriptor);
+// final CloudletDescriptor deserialized = gson.fromJson(asJson, CloudletDescriptor.class);
 
-        assertEquals(cloudletDescriptor, deserialized);
-    }
+// assertEquals(cloudletDescriptor, deserialized);
+// }
 
-    @Test
-    public void serializesDeserializesListToJSON() {
-        final CloudletDescriptor cloudletDescriptor1 = new CloudletDescriptor(1, 2, 3, 4);
-        final CloudletDescriptor cloudletDescriptor5 = new CloudletDescriptor(5, 6, 7, 8);
+// @Test
+// public void serializesDeserializesListToJSON() {
+// final CloudletDescriptor cloudletDescriptor1 = new CloudletDescriptor(1, 2, 3, 4);
+// final CloudletDescriptor cloudletDescriptor5 = new CloudletDescriptor(5, 6, 7, 8);
 
-        final List<CloudletDescriptor> cloudletDescriptors =
-                Arrays.asList(cloudletDescriptor1, cloudletDescriptor5);
+// final List<CloudletDescriptor> cloudletDescriptors =
+// Arrays.asList(cloudletDescriptor1, cloudletDescriptor5);
 
-        final String serialized = gson.toJson(cloudletDescriptors);
+// final String serialized = gson.toJson(cloudletDescriptors);
 
-        Type listType = new TypeToken<List<CloudletDescriptor>>() {}.getType();
+// Type listType = new TypeToken<List<CloudletDescriptor>>() {}.getType();
 
-        final List<CloudletDescriptor> deserialized = gson.fromJson(serialized, listType);
+// final List<CloudletDescriptor> deserialized = gson.fromJson(serialized, listType);
 
-        System.out.println(deserialized);
+// System.out.println(deserialized);
 
-        assertEquals(2, deserialized.size());
-    }
+// assertEquals(2, deserialized.size());
+// }
 
-    @AfterAll
-    public static void deleteJobLogDirectory() {
-        // Recursively delete the tempDirectory and its contents
-        try {
-            FileUtils.deleteDirectory(new File("./logs"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+// @AfterAll
+// public static void deleteJobLogDirectory() {
+// // Recursively delete the tempDirectory and its contents
+// try {
+// FileUtils.deleteDirectory(new File("./logs"));
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
+// }
